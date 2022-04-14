b67ef2237ad75d00774243c4bcd27807a38e0c78
Daniel Juarez <juarezdaniel@gmail.com>
2022-04-14 12:22:48 -0400

    hexicomb

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,7 +10,35 @@ function Resume() {
                 <p>OBJECTIVE</p>
                 <p>PROFILE STATEMENT</p>
                 <p>EDUCATION</p>
+                <ul>
+                    <li>Carolina Code School</li>
+                        <ul>
+                            <li>Certificate - Web Development Immersive Course, 444 hours</li>
+                        </ul>
+                    <li>Greenville Technical College</li>
+                        <ul>
+                            <li>Second Year Student, Major: Business Management</li>
+                        </ul>
+                    <li>High School</li>
+                        <ul>
+                            <li>Bradley Academy, HS Diplima - GPA 4.671</li>
+                        </ul>
+                </ul>
                 <p>WORK EXPERIENCE</p>
+                    <ul>
+                        <li>Enok Collective Inc.</li>
+                            <ul>
+                                <li>Product Manager/Technical Account Manager: July 2019 - Current</li>
+                            </ul>
+                        <li>Q2 Management Inc.</li>
+                            <ul>
+                                <li>Regional Supervisor: June 2018 - June 2019</li>
+                            </ul>
+                        <li>General Information Services</li>
+                            <ul>
+                                <li>Veri</li>
+                            </ul>
+                    </ul>
                 <p>LEADERSHIP TRAINING</p>
                 <p>Military Training</p>
                 <ul>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,40 @@
 function Projects() {
     return (
         <>
-        <div className="projectIcon iconOne">I am the projects page</div>
-        <div className="projectIcon iconTwo">I am the projects page</div>
-        <div className="projectIcon iconThree">I am the projects page</div>
-        <div className="projectIcon iconFour">I am the projects page</div>
-        <div className="projectIcon iconFive">I am the projects page</div>
+            <div className="top">
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+            </div>
+            <div className="middle-top">
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+            </div>
+            <div className="middle-bottom">
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+            </div>
+            <div className="bottom">
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+                <div className="hex"></div>
+            </div>
         </>
     )
 }
