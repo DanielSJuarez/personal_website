f62225366ccb5209e5e2e64608660a2566bc2121
Daniel Juarez <juarezdaniel@gmail.com>
2022-04-14 15:35:54 -0400

    content

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,13 @@
 function Home() {
     return (
         <>
-            <h1>Hello, sadflajsf;laksjdf;alksjfas</h1>
+            <h1 className="Title">Hello! Welcome to juarezdaniel.com</h1>
             <div className="imageHolder">
                 <img className='image' src='' alt="profileImage" />
             </div>
-            <p>Daniel Juarez</p>
-            <p>About Me:</p>
-            <p>a;sdlfjas;ldkfja;lsgkjasd;lgkaj</p>
+            <p className="sectionHeaders">Daniel Juarez</p>
+            <p className="sectionSubHeaders">About Me:</p>
+            <p className="introText">Hello! Welcome! My name is Daniel Juarez, and I am glad that you are here. I am a junior developer for Enok Collective starting in 2022. I completed training at an awesome school call Carolina Code School, where I completed their full-stack immersive course. But where did I start? I actually was a Product Manager and Technical Account Manager at Enok Collective for 3 year. Though, this was a great position, it did not fulfill my urge for continued improvment, problem-solving, and creativitey in that way I would like. I still had a love for the tech industry and becomming a software engineer seemed like a create move. A year later, I have no regrets and my journey of learning continues to grow and blosum. Feel free to travel around my site to learn more about me. I also would love to connect with you on social media!!! Browse my projects and let me know if you have questions, or feedback. I look forward to hearing you!!</p>
         </>
     )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,38 +2,55 @@ function Projects() {
     return (
         <>
             <div className="top">
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
+                <div className="hex first top-animation"></div>
+                <div className="hex right-animation"></div>
+                <div className="top-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/pixel-perfect')}>CSS-Pixel Perfect</div>
+                </div>
+                <div className="hex bottom-animation"></div>
+                <div className="hex left-animation "></div>
+                <div className="hex right-animation"></div>
+                <div className="hex left-animation"></div>
+                <div className="hex top-animation"></div>
             </div>
             <div className="middle-top">
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
+                <div className="hexAlt bottom-animation"></div>
+                <div className="hexAlt top-animation"></div>
+                <div className="hexAlt bottom-animation"></div>
+                <div className="left-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/js-war-game')}>JS-War Game</div>
+                </div>
+                <div className="hexAlt right-animation"></div>
+                <div className="top-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/slackalack-app')}>Slackalacka</div>
+                </div>
+                <div className="hexAlt bottom-animation"></div>
             </div>
             <div className="middle-bottom">
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
+                <div className="first left-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/full-stack-resturant-app')}>Restaurant App</div>
+                </div>
+                <div className="hex bottom-animation"></div>
+                <div className="left-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/python_wordle')}>Python Wordle</div>
+                </div>
+                <div className="hex right-animation"></div>
+                <div className="top-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/safehandsdoula-app')}>Safe Hands Doula</div>
+                </div>
+                <div className="hex left-animation"></div>
+                <div className="hex top-animation"></div>
             </div>
             <div className="bottom">
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
-                <div className="hex"></div>
+                <div className="hexAlt right-animation linkHex">
+                    <div className="textDiv"  onClick={() =>  window.open('https://github.com/DanielSJuarez/fullstack-news-app')}>News App</div>
+                </div>
+                <div className="hexAlt top-animation"></div>
+                <div className="hexAlt bottom-animation"></div>
+                <div className="hexAlt top-animation"></div>
+                <div className="hexAlt right-animation"></div>
+                <div className="hexAlt bottom-animation"></div>
+                <div className="hexAlt left-animation"></div>
             </div>
         </>
     )
